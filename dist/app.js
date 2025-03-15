9b53466f72f684973cf5e0dbab2b72ab413169d7
ARRGUPT <guptafamily3005@gmail.com>
2025-03-15 09:13:01 +0530

    cors error

--- dist/app.js
+++ dist/app.js
@@ -42,6 +42,7 @@ async function fetchAndDisplayAdvice(temperature, rainfall, windSpeed, humidity)
 
   try {
     const response = await fetch("https://weatherwise-api.onrender.com/predict", {
+      mode: "no-cors",
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
